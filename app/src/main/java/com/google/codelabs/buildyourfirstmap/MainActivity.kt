2aedf9c9b1bf46c37a616878f46b6195669c023c
Your Name <daryagrichenok@gmail.com>
2023-02-15 15:24:40 +0300

    GoogleMap (Customize markers, cluster markers, draw on the map, camera control)

---
+++ app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.google.codelabs.buildyourfirstmap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.codelabs.buildyourfirstmap.place.Place
+import com.google.codelabs.buildyourfirstmap.place.PlaceRenderer
+import com.google.codelabs.buildyourfirstmap.place.PlacesReader
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.ktx.addCircle
+import com.google.maps.android.ktx.addMarker
+import com.google.maps.android.ktx.awaitMap
+import com.google.maps.android.ktx.awaitMapLoad
+
+class MainActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy { PlacesReader(this).read() }
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.colorPrimary)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_directions_bike_black_24dp, color)
+    }
+    private var circle: Circle? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        lifecycleScope.launchWhenCreated {
+            val googleMap = mapFragment.awaitMap()
+            googleMap.awaitMapLoad()
+            val bounds = LatLngBounds.builder()
+            places.forEach { bounds.include(it.latLng) }
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20))
+
+            addClusteredMarkers(googleMap)
+        }
+    }
+
+    private fun addClusteredMarkers(googleMap: GoogleMap) {
+        val clusterManager = ClusterManager<Place>(this, googleMap)
+        clusterManager.renderer = PlaceRenderer(this, googleMap, clusterManager)
+        clusterManager.markerCollection.setInfoWindowAdapter(MarkerInfoWindowAdapter(this))
+        clusterManager.addItems(places)
+        clusterManager.cluster()
+        clusterManager.setOnClusterItemClickListener { item ->
+            addCircle(googleMap, item)
+            return@setOnClusterItemClickListener false
+        }
+        googleMap.setOnCameraMoveStartedListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 0.3f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 0.3f }
+        }
+
+        googleMap.setOnCameraIdleListener {
+            clusterManager.markerCollection.markers.forEach { it.alpha = 1.0f }
+            clusterManager.clusterMarkerCollection.markers.forEach { it.alpha = 1.0f }
+
+            clusterManager.onCameraIdle()
+        }
+    }
+
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker {
+                title(place.name)
+                position(place.latLng)
+                icon(bicycleIcon)
+            }
+            marker.tag = place
+        }
+    }
+
+    private fun addCircle(googleMap: GoogleMap, item: Place) {
+        circle?.remove()
+        circle = googleMap.addCircle {
+            center(item.latLng)
+            radius(1000.0)
+            fillColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimaryTranslucent))
+            strokeColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
+        }
+    }
+}
+
+
+
+
+
+
+
